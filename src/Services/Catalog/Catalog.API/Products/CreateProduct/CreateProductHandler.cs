64fcd61503fd5208d9f99d68502892faf440fd99
teomihov <tmihov89@gmail.com>
2024-12-03 22:24:23 +0200

    carter and mediatr abstraction. Create first command handler structure

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,15 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price) 
+    : ICommand<CreateProductResult>;
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
