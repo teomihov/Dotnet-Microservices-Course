0c8ee702d39579ea5c1f6b53d0afc8d92e71f716
teomihov <tmihov89@gmail.com>
2024-12-18 10:24:08 +0200

    add base exceptions

---
+++ src/BuildingBlocks/Exceptions/NotFoundException.cs
@@ -0,0 +1,9 @@
+﻿namespace BuildingBlocks.Exceptions;
+internal class NotFoundException : Exception
+{
+    public NotFoundException(string message) : base(message) { }
+
+    public NotFoundException(string name, object id) : base($"The entity {name} with an ID: {id} is not found")
+    {
+    }
+}
