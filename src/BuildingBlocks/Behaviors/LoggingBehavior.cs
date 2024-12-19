5dcbe3ea05931ab104b069ded01bcba485b3a84a
teomihov <tmihov89@gmail.com>
2024-12-19 15:21:19 +0200

    logging behavior + postgres health check

---
+++ src/BuildingBlocks/Behaviors/LoggingBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+
+using MediatR;
+
+using Microsoft.Extensions.Logging;
+
+namespace BuildingBlocks.Behaviors;
+
+public class LoggingBehavior<TRequest, TResponse>(ILogger<LoggingBehavior<TRequest, TResponse>> logger)
+    : IPipelineBehavior<TRequest, TResponse>
+    where TRequest : notnull, IRequest<TResponse>
+    where TResponse : notnull
+{
+    public async Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
+    {
+        logger.LogInformation($"[Start] Handle request={typeof(TRequest).Name} - Response={typeof(TResponse).Name}");
+
+        var timer = new Stopwatch();
+        timer.Start();
+
+        var response = await next();
+
+        timer.Stop();
+        
+        var timeTaken = timer.Elapsed;
+        if (timeTaken.Seconds > 3)
+        {
+            logger.LogWarning($"[Permormance] The request {typeof(TRequest).Name} took {timeTaken}");
+        }
+
+        return response;
+    }
+}
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,12 +1,15 @@
 using BuildingBlocks.Behaviors;
 using BuildingBlocks.Exceptions.Handlers;
 
+using Catalog.API.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddMediatR(config =>
 {
     config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
     config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
 });
 builder.Services.AddValidatorsFromAssembly(typeof(Program).Assembly);
 
@@ -17,7 +20,14 @@ builder.Services.AddMarten(opts =>
     opts.Connection(builder.Configuration.GetConnectionString("Database")!);
 }).UseLightweightSessions();
 
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.InitializeMartenWith<CatalogInitialData>();
+}
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks().AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 // Configure the HTTP request pipeline.
 var app = builder.Build();
@@ -25,5 +35,6 @@ var app = builder.Build();
 app.MapCarter();
 
 app.UseExceptionHandler(options => { });
+app.UseHealthChecks("/health");
 
 app.Run();
